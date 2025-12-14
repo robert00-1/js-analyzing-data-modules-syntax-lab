dff52d232bb529f0299561b6c45be9071b3043cd
robert01mm <mmasirobert08@gmail.com>
2025-12-14 07:44:43 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+  for (const userArray of args) {
+    combinedObject.users.push(...userArray);
 
+  }
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+    
+
+  return combinedObject;
+}
 
 
 module.exports = {
